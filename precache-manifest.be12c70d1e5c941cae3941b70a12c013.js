171614ebe878d3bcced3df1d25664b154b06e519
Vaibhav Ganju <vaibhav34@gmail.com>
2021-10-06 00:59:35 +0530

    Updates

---
+++ precache-manifest.be12c70d1e5c941cae3941b70a12c013.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a5e9588993fccd91bb87ed0bc191c5",
+    "url": "/planetM-work/index.html"
+  },
+  {
+    "revision": "79c5abc343a63a7e3748",
+    "url": "/planetM-work/static/css/main.bbc01d94.chunk.css"
+  },
+  {
+    "revision": "6c010d43f9e614f841f3",
+    "url": "/planetM-work/static/js/2.bb598dab.chunk.js"
+  },
+  {
+    "revision": "79c5abc343a63a7e3748",
+    "url": "/planetM-work/static/js/main.f7415592.chunk.js"
+  },
+  {
+    "revision": "e16bef6b6123b514e249",
+    "url": "/planetM-work/static/js/runtime-main.14fe480b.js"
+  }
+]);
